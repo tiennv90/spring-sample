b35d35adc34d5ecec7245e53083549e1a5479ad3
tiennv90 <tiennv90@gmail.com>
2013-11-06 22:24:49 +0700

    add JSON object to ClientController

--- src/main/java/com/spring/sample/controller/ClientController.java
+++ src/main/java/com/spring/sample/controller/ClientController.java
@@ -2,6 +2,8 @@ package com.spring.sample.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.spring.sample.dao.ClientDAO;
 import com.spring.sample.model.Client;
+import com.spring.sample.model.json.JSONClient;
 
 import flexjson.JSONSerializer;
 
@@ -20,11 +23,18 @@ public class ClientController {
 	@Autowired
 	private ClientDAO clientDAO;
 	
-	@RequestMapping(value = "/getall", method = RequestMethod.GET)
-	public @ResponseBody String getAllClient() {
+	@RequestMapping(method = RequestMethod.GET)
+	public @ResponseBody String getAllClient(HttpServletRequest request) {
+		
 		List<Client> clients = clientDAO.findAll();
-		String json = new JSONSerializer().serialize(clients);
-		return json;
+		
+		JSONClient jsonClients = new JSONClient(clients);
+		
+		String restOfTheUrl = request.getRequestURL().toString();
+		jsonClients.setTotalCount(clients != null ? clients.size() : 0);
+		jsonClients.setHref(restOfTheUrl);
+		
+		return new JSONSerializer().exclude("*.class").deepSerialize(jsonClients);
 	}
 
 }
