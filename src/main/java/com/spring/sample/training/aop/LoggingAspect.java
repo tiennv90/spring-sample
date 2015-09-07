5974b2f1018c18e92dfd836130c12b074810d03c
tiennv <tiennv90@gmail.com>
2015-09-07 16:36:59 +0700

    update advice example

--- src/main/java/com/spring/sample/training/aop/LoggingAspect.java
+++ src/main/java/com/spring/sample/training/aop/LoggingAspect.java
@@ -6,8 +6,18 @@ import org.aspectj.lang.annotation.Before;
 @Aspect
 public class LoggingAspect {
 	
-	@Before("execution (public String getName())")
+	@Before("execution (public String com.spring.sample.training.aop.model.Shape.getName())")
 	public void loggingAdvice() {
 		System.out.println("Advice run. Get method called");
+	}
+	
+	@Before("execution (public * get*())")
+	public void loggingGlobalAdvice() {
+		System.out.println("Global Advice run. Get method called");
+	}
+	
+	@Before("execution ( * get*())")
+	public void loggingSecondAdvice() {
+		System.out.println("Second advice executed");
 	}
 }
