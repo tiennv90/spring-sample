49512c2d7877a98abbc5b6787f9d114ca1024055
tiennv <tiennv90@gmail.com>
2015-09-12 11:23:46 +0700

    finish Track counter example

--- src/main/java/com/spring/sample/training/aop/track/CompactDisc.java
+++ src/main/java/com/spring/sample/training/aop/track/CompactDisc.java
@@ -1,9 +1,40 @@
 package com.spring.sample.training.aop.track;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CompactDisc {
+
+	private String title;
+	private List<String> tracks = new ArrayList<String>();
+	private String artist;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public List<String> getTracks() {
+		return tracks;
+	}
+
+	public void setTracks(List<String> tracks) {
+		this.tracks = tracks;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
 	
 	public void playTrack(int trackNumber) {
-		System.out.println("Play Track " + trackNumber);
+		System.out.println("Playing Track Id: " + trackNumber + " name: " + tracks.get(trackNumber));
 	}
 	
 }
--- src/main/java/com/spring/sample/training/aop/track/TrackCounter.java
+++ src/main/java/com/spring/sample/training/aop/track/TrackCounter.java
@@ -9,19 +9,19 @@ import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class TrackCounter {
-	
 	private Map<Integer, Integer> trackCounts = new HashMap<Integer, Integer>();
-	
-	@Pointcut("execution(com.spring.sample.training.aop.track.CompactDisk.playTrack(int)) && args(trackNumber)")
-	public void trackPlayed(int trackNumber) {}
-	
+
+	@Pointcut("execution(* com.spring.sample.training.aop.track.CompactDisc.playTrack(int)) " + "&& args(trackNumber)")
+	public void trackPlayed(int trackNumber) {
+	}
+
 	@Before("trackPlayed(trackNumber)")
 	public void countTrack(int trackNumber) {
-		
-		int count = 0;
-		if (trackCounts.containsKey(trackNumber)) {
-			count = trackCounts.get(trackNumber);
-		}
-		trackCounts.put(trackNumber, count + 1);
+		int currentCount = getPlayCount(trackNumber);
+		trackCounts.put(trackNumber, currentCount + 1);
+	}
+
+	public int getPlayCount(int trackNumber) {
+		return trackCounts.containsKey(trackNumber) ? trackCounts.get(trackNumber) : 0;
 	}
 }
